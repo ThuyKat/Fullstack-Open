d8bd8644363bfdb43e3e059f02390a0d9a9a5d6c
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-03-25 14:25:28 +1100

    unicafe exercise solution

---
+++ part1/unicafe/unicafe/src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+function App() {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button
+        onClick={() => {
+          setGood(good + 1);
+        }}
+      >
+        good
+      </button>
+      <button
+        onClick={() => {
+          setNeutral(neutral + 1);
+        }}
+      >
+        neutral
+      </button>
+      <button
+        onClick={() => {
+          setBad(bad + 1);
+        }}
+      >
+        bad
+      </button>
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+}
+
+export default App;
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  let all = good + bad + neutral;
+  let average = (good * 1 + neutral * 0 + bad * -1) / all;
+  let positive = good / all;
+  if (all === 0) {
+    return <p>No feedback given</p>;
+  }
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <StatisticLine text="good" value={good} />
+        </tr>
+        <tr>
+          <StatisticLine text="neutral" value={neutral} />
+        </tr>
+        <tr>
+          <StatisticLine text="bad" value={bad} />
+        </tr>
+        <tr>
+          <StatisticLine text="all" value={all} />
+        </tr>
+        <tr>
+          <StatisticLine text="average" value={average} />
+        </tr>
+        <tr>
+          <StatisticLine text="positive" value={positive} />
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+const StatisticLine = ({ text, value }) => {
+  return (
+    <>
+      <td>{text}</td>
+      <td>{value}</td>
+    </>
+  );
+};
